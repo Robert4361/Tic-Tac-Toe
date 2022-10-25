29fa416034d4446dc785fc83145a8aef1f94efe0
Robert4361 <robert4361@gmail.com>
2022-10-25 17:01:33 +0200

    Improve file structure

--- board.rb
+++ board.rb
@@ -1,15 +1,5 @@
 # frozen_string_literal: true
 
-# this class is used for creating players and assigning symbols for the players
-class Player
-  attr_reader :name, :symbol
-
-  def initialize(name, symbol)
-    @name = name
-    @symbol = symbol
-  end
-end
-
 # this class is used for printing the game area, and for putting player symbols
 class Board
   attr_reader :area
---
+++ game.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require './player'
+require './board'
+
+class Game
+end
