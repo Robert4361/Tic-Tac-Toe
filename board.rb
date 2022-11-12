6336216bd5b04f828f38e0ea8b6bf33a9327b6e5
Robert4361 <robert4361@gmail.com>
2022-11-12 18:35:19 +0100

    Change board to 2d instead of 1d array

--- board.rb
+++ board.rb
@@ -5,18 +5,19 @@ class Board
   attr_reader :area
 
   def initialize
-    @area = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @area = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
   end
 
   def swap(number, symbol)
-    @area[@area.index(number)] = symbol
+    row = @area.index { |rows| rows.index(number) }
+    @area[row][@area[row].index(number)] = symbol
   end
 
-  def print_board
-    print "| #{@area[0]} | #{@area[1]} | #{area[2]} |\n"
+  def print_board # rubocop:disable Metrics/AbcSize
+    print "| #{@area[0][0]} | #{@area[0][1]} | #{area[0][2]} |\n"
     print "-------------\n"
-    print "| #{@area[3]} | #{@area[4]} | #{area[5]} |\n"
+    print "| #{@area[1][0]} | #{@area[1][1]} | #{area[1][2]} |\n"
     print "-------------\n"
-    print "| #{@area[6]} | #{@area[7]} | #{area[8]} |\n"
+    print "| #{@area[2][0]} | #{@area[2][1]} | #{area[2][2]} |\n"
   end
 end
